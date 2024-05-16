09bbe869f26bbc4ab95408c5d4ae0673aeae61bb
Feedbaek <nicek789@naver.com>
2024-05-16 15:02:00 +0900

    Refactor: 커스텀 클래스를 사용한 예외처리

--- src/main/java/Dino/Duett/global/exception/GlobalExceptionHandler.java
+++ src/main/java/Dino/Duett/global/exception/GlobalExceptionHandler.java
@@ -14,36 +14,6 @@ import java.util.Map;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    public static final int METHOD_ARGUMENT_FIRST_ERROR_INDEX = 0;
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleRequestValidationException(
-        final MethodArgumentNotValidException e
-    ) {
-        final FieldError firstErrorField = e.getFieldErrors()
-            .get(METHOD_ARGUMENT_FIRST_ERROR_INDEX);
-        final CustomException exception = CustomException.fromFieldError(firstErrorField);
-
-        log.error(exception.getErrorInfoLog());
-
-        return ResponseEntity.badRequest().body(ErrorResponse.from(exception));
-    }
-
-    @ExceptionHandler(MissingPathVariableException.class)
-    public ResponseEntity<ErrorResponse> handleEmptyPathVariableException(
-        final MissingPathVariableException e
-    ) {
-        final CustomException exception = CustomException.of(
-            ErrorCode.WRONG_REQUEST_URL,
-            Map.of(e.getVariableName(), "")
-        );
-
-        log.error(exception.getErrorInfoLog());
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.from(exception));
-    }
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleInternalServer(final Exception e) {
         final CustomException customException = CustomException.from(
